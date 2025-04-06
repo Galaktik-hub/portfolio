6efee23ba9c3148a16ab43bad9ea50fd8284bc96
Galaktik-hub <dvatheking@gmail.com>
2025-04-06 14:23:06 +0200

    Added target _blank fields

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,17 +10,17 @@ const Header: React.FC = () => {
                         <a href="#contact">Contact</a>
                     </li>
                     <li>
-                        <a href="https://www.linkedin.com/in/alexis-telle/" id="linkedin">
+                        <a href="https://www.linkedin.com/in/alexis-telle/" target={"_blank"} id="linkedin">
                             <img src="../../public/linkedin.svg" alt="LinkedIn" />
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/Galaktik-hub" id="github">
+                        <a href="https://github.com/Galaktik-hub" target={"_blank"} id="github">
                             <img src="../../public/github.svg" alt="GitHub" />
                         </a>
                     </li>
                     <li>
-                        <a href="../../public/TELLE%20Alexis_CV.pdf">CV</a>
+                        <a href="../../public/TELLE%20Alexis_CV.pdf" target={"_blank"}>CV</a>
                     </li>
                 </ul>
             </nav>
